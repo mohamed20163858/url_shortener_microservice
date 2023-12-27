93b0928156913e156f6ec667995f63d641426315
Mohamed Saleh <mohamed20163858@gmail.com>
2023-12-27 22:06:13 +0200

    Create URL shortener microservice

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose"); 
 const app = express();
-
+function isValidURL(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlShorterSchema = new mongoose.Schema({
+  shortUrl: Number,
+  originalUrl: String
+})
+
+let UrlShorter = mongoose.model("UrlShorter", urlShorterSchema);
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +37,40 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post("/api/shorturl", async (req, res) => {
+  const originalUrl = req.body.url;
+  if(isValidURL(originalUrl))
+  {
+    const check = await UrlShorter.exists({ originalUrl:  originalUrl}, { new: true});
+    let obj;
+    let Url;
+    console.log("check: ", check);
+    if(check) {
+      Url = await UrlShorter.findById(check);
+      obj = {original_url: originalUrl, shorter_url: Url.shortUrl }
+    }
+    else {
+      const count = await UrlShorter.countDocuments();
+      obj = {original_url: originalUrl, shorter_url: count}
+      await UrlShorter.create({originalUrl: originalUrl, shortUrl: count});
+    }
+    res.json(obj);
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+
+});
+
+app.get("/api/shorturl/:id", async (req, res) => {
+  const id = req.params.id;
+  const url =  await UrlShorter.findOne({shortUrl: id});
+  if(url) {
+    res.redirect(url.originalUrl);
+  }
+  else {
+    res.redirect("/");
+  }
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
